7da60ece5830c4b2d94948621c0a093294939d87
ocralo <ocralo@gmail.com>
2023-08-05 22:49:00 -0500

    fix: correction import component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 	import { RouterView } from 'vue-router'
-	import CardFlip from '@/components/CardFlip/index.vue'
 </script>
 
 <template>
 	<RouterView />
-	<CardFlip />
 </template>
