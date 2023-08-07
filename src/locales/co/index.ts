245091e9220f4f08724685a5e5e93acb495be748
ocralo <ocralo@gmail.com>
2023-08-07 11:51:40 -0500

    feat: add texts to co and us

--- src/locales/co/index.ts
+++ src/locales/co/index.ts
@@ -1,1 +1,5 @@
-export default {}
+import home from '@/locales/co/home.json'
+import cardGame from '@/locales/co/card-game.json'
+import modal from '@/locales/co/modal.json'
+import tableCardFlip from '@/locales/co/table-card-flip.json'
+export default { home, cardGame, modal, tableCardFlip }
