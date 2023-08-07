5ebf0c988d70a7919d8f5f2b03eddbbfea692af3
ocralo <ocralo@gmail.com>
2023-08-07 01:52:01 -0500

    feat: add tests

---
+++ src/pages/Home/__tests__/index.spec.ts
@@ -0,0 +1,58 @@
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import sinon from 'sinon'
+import { createTestingPinia } from '@pinia/testing'
+import { useUserStore } from '@/stores/user'
+
+import Home from '@/pages/Home/index.vue'
+
+const push = vi.fn()
+
+vi.mock('vue-router', () => ({
+	useRoute: vi.fn(),
+	useRouter: vi.fn(() => ({
+		push,
+	})),
+}))
+
+describe('Home', () => {
+	let wrapper: any
+	let store: any
+
+	beforeEach(() => {
+		wrapper = shallowMount(Home, {
+			global: {
+				plugins: [createTestingPinia({ createSpy: sinon.spy })],
+			},
+		})
+
+		store = useUserStore()
+	})
+
+	afterEach(() => {
+		wrapper.unmount()
+	})
+
+	describe('mounted', () => {
+		it('should render', () => {
+			expect(wrapper.exists()).toBeTruthy()
+		})
+	})
+
+	describe('methods', () => {
+		describe('handleSubmit', () => {
+			it('should not call setUser', () => {
+				wrapper.vm.handleSubmit()
+				expect(store.setUser.calledOnce).toBeFalsy()
+			})
+			it('should call setUser', () => {
+				wrapper.vm.user = 'test'
+				wrapper.vm.numberOfPairs = 4
+				wrapper.vm.handleSubmit()
+				expect(store.setUser.calledOnce).toBeTruthy()
+				expect(push).toHaveBeenCalled()
+				expect(push).toHaveBeenCalledWith('/card-game')
+			})
+		})
+	})
+})
