8d5ce8112697d3174ef29c75b4f148273aa1528c
ocralo <ocralo@gmail.com>
2023-08-07 18:06:27 -0500

    feat: update texts

--- src/locales/co/index.ts
+++ src/locales/co/index.ts
@@ -2,4 +2,6 @@ import home from '@/locales/co/home.json'
 import cardGame from '@/locales/co/card-game.json'
 import modal from '@/locales/co/modal.json'
 import tableCardFlip from '@/locales/co/table-card-flip.json'
-export default { home, cardGame, modal, tableCardFlip }
+import switchTranslate from '@/locales/co/switch-translate.json'
+
+export default { home, cardGame, modal, tableCardFlip, switchTranslate }
--- src/locales/us/index.ts
+++ src/locales/us/index.ts
@@ -2,4 +2,6 @@ import home from '@/locales/us/home.json'
 import cardGame from '@/locales/us/card-game.json'
 import modal from '@/locales/us/modal.json'
 import tableCardFlip from '@/locales/us/table-card-flip.json'
-export default { home, cardGame, modal, tableCardFlip }
+import switchTranslate from '@/locales/us/switch-translate.json'
+
+export default { home, cardGame, modal, tableCardFlip, switchTranslate }
