c5c203de6af6bdf7276a7f8b6b1b6b1c19f3299c
ocralo <ocralo@gmail.com>
2023-08-07 18:07:35 -0500

    feat: use component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 	import { RouterView } from 'vue-router'
+	import SwitchTranslate from '@/containers/SwitchTranslate/index.vue'
 </script>
 
 <template>
+	<SwitchTranslate class="mt-4 ml-4" />
 	<RouterView />
 </template>
